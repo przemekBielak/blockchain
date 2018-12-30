902ebe75bf5ea87c921d92dd5fa99fe0e8820e78
Przemek Bielak <przemyslaw.bielak@protonmail.com>
2018-12-30 16:56:20 +0100

    added cyclic task and reading of txt file

--- blockchain.py
+++ blockchain.py
@@ -1,8 +1,17 @@
+#!/usr/bin/python3
+
+import sys
+import random
+import sched
+import time
+
 BALANCES = {
     'user0': 1000,
     'user1': 2000,
     'user2': 3000,
 }
+
+PORT = 0
 
 
 def add_user(user):
@@ -19,9 +28,39 @@ def transfer(src, dst, amount):
         BALANCES[src] -= amount
 
 
-print(BALANCES)
-add_user('user3')
+# get random line from txt file
+def get_random_line():
+    file_path = './list.txt'
+    test_file = open(file_path, 'r')
+    lines = test_file.read().split('\n')
+    return random.choice(lines)
 
-transfer('user1', 'user3', 2500)
 
-print(BALANCES)
+def print_random_line(text_to_print):
+    print(get_random_line())
+    print(text_to_print)
+
+
+def run_scheduler():
+    while True:
+        # call function cyclically
+        s = sched.scheduler(time.time, time.sleep)
+        s.enter(1, 1, print_random_line, argument=('argument_test',))
+        s.run()
+
+
+def main(argv):
+    # get port from command line argument
+    PORT = argv[1]
+    print('port: ' + PORT)
+
+    print(BALANCES)
+    add_user('user3')
+    transfer('user1', 'user3', 2500)
+    print(BALANCES)
+
+    run_scheduler()
+
+
+if __name__ == '__main__':
+    main(sys.argv)
