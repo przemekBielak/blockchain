51d13f6084b83d0461cb8a7794c25f0447d0a181
Przemek Bielak <przemyslaw.bielak@protonmail.com>
2019-01-05 19:52:03 +0100

    removed test user data, added basic blockchain generation functions

--- blockchain.go
+++ blockchain.go
@@ -3,59 +3,13 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
-	"strings"
-	"math/rand"
-	"time"
 	"net/http"
 	"net/url"
 	"strconv"
 )
 
-type userDataT struct {
-	data string
-	version int
-} 
-
-
-func addUser(balances map[string]float64, user string) {
-	balances[user] = 0
-}
-
-func getBalance(balances map[string]float64, user string) float64 {
-	return balances[user]
-}
-
-func transfer(balances map[string]float64, src string, dst string, amount float64) {
-	if balances[src] >= amount {
-		balances[dst] += amount
-		balances[src] -= amount
-	}
-}
-
-func (userData *userDataT) updateUserData() {
-	// read file
-	dat, err := ioutil.ReadFile("./list.txt")
-	if err != nil {
-		panic(err)
-	}
-	fileContent := string(dat)
-	lines := strings.Split(fileContent, "\n")
-
-	// update random seed
-	rand.Seed(time.Now().UnixNano())
-
-	// update data with random line from txt file and increment version
-	userData.data = lines[rand.Intn(len(lines))]
-	userData.version++
-}
-
 func main() {
-
-	userData := userDataT{}
-	userData.updateUserData()
-	fmt.Println(userData.data, strconv.Itoa(userData.version))
-
-	resp, err := http.PostForm("http://localhost:7000/hello", url.Values{"data": {userData.data}, "version": {strconv.Itoa(userData.version)}})
+	resp, err := http.PostForm("http://localhost:7000/hello", url.Values{"data": {"test data"}, "version": {strconv.Itoa(1)}})
 	if err != nil {
 		fmt.Println("ERROR:", err)
 	}
--- server.go
+++ server.go
@@ -5,10 +5,97 @@ import (
 	"fmt"
 	"log"
 	"strings"
+	"strconv"
 	"reflect"
+	"crypto/sha256"
+	"encoding/hex"
+	"time"
+	"errors"
 )
 
+type block struct {
+	index int
+	timestamp string
+	data string
+	prevHash string
+	hash string
+}
+
+var blockchain []block
+
+func calculateHash(block block) string {
+	hash := sha256.New()
+	dataToHash := strconv.Itoa(block.index) + block.timestamp + block.data + block.prevHash
+	hash.Write([]byte(dataToHash))
+	hashed := hash.Sum(nil)
+
+	return hex.EncodeToString(hashed)
+}
+
+func generateBlock(prevBlock block, data string) block {
+	var newBlock block
+	newBlock.index = prevBlock.index + 1
+	newBlock.timestamp = time.Now().String()
+	newBlock.data = data
+	newBlock.prevHash = prevBlock.hash
+	newBlock.hash = calculateHash(newBlock)
+
+	return newBlock
+}
+
+// false - block invalid, true - block valid
+func validateBlock(prevBlock block, newBlock block) bool {
+	retVal := true
+
+	if newBlock.index != prevBlock.index + 1 {
+		retVal = false
+	} else if newBlock.prevHash != prevBlock.hash {
+		retVal = false
+	}
+
+	return retVal
+}
+
+func appendToBlockchain(block block) error {
+	var err error
+	if validateBlock(blockchain[len(blockchain) - 1], block) {
+		blockchain = append(blockchain, block)
+	} else {
+		err = errors.New("Adding to blockchain failed")
+	}
+
+	return err
+}
+
 func main() {
+	// create genesis block 
+	blockchain = append(blockchain, block{0, "genesis", "genesis", "genesis", "genesis"})
+	fmt.Println(blockchain[0])
+
+	block1 := generateBlock(blockchain[0], "data block 1")
+	err := appendToBlockchain(block1)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(blockchain[len(blockchain) - 1])
+	}
+
+	block2 := generateBlock(block1, "data block 2")
+	err = appendToBlockchain(block2)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(block2)
+	}
+
+	err = appendToBlockchain(generateBlock(block2, "data block 3"))
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(blockchain[len(blockchain) - 1])
+	}
+
+	// create server
 	http.HandleFunc("/hello", handlePost)
 	fmt.Println("Serving on port http//localhost:7000/hello")
 	log.Fatal(http.ListenAndServe("localhost:7000", nil))
@@ -28,8 +115,6 @@ func handlePost(w http.ResponseWriter, req *http.Request) {
 	fmt.Println("Type of data:", reflect.TypeOf(receivedData))
 	fmt.Println("Type of version:", reflect.TypeOf(receivedVersion))
 
-
+	// send back data
 	fmt.Fprintf(w, "Data: %s, Version: %s", strings.ToUpper(receivedData), receivedVersion)
-
-
 }
